6b0cb48a0134d78d0af651262d7144908837e517
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-24 18:37:12 +0530

    Update and rename LionZ.py to Lucifer.py

---
+++ Lucifer.py
@@ -0,0 +1,13 @@
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clone https://gitHub.com/kaal0408/LuciferUserbot && cd LuciferUserbot && python3 -m userbot")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Lucifer_support_group")
+# Script written for stop heroku suspension
+# use with credit else gay
