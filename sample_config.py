a0a0298c7c767245682ab1d1379db3071682cb2f
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-26 19:06:32 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Kolkata")
     # set this with required lion repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/TeamLionX/LionX.git"
+        "UPSTREAM_REPO", "https://github.com/kaal0408/LuciferUserbot.git"
     )
 
     # BASIC and MAIN CONFIG VARS
