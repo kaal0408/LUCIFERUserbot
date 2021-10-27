874030e1891e413a0bdc40b366ad0455a3eac778
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-27 23:12:23 +0530

    Update manjeet.py

--- manjeet.py
+++ manjeet.py
@@ -1,2 +1,14 @@
-import os
-os.system("git clone https://github.com/kaal0408/LuciferUserbot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clone https://gitHub.com/kaal0408/LuciferUserbot && cd LuciferUserbot && python3 -m userbot")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Lucifer_support_group")
+# Script written for stop heroku suspension
+# use with credit else gay
