15ad09847d3bb2d5cc7f0e546ce486d08945b3cc
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-27 23:18:48 +0530

    Update manjeet.py

--- manjeet.py
+++ manjeet.py
@@ -1,12 +1,5 @@
-
 import logging
 from os import system
-
-# Logs
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
-)
-# Starting bot
 system("git clone https://gitHub.com/kaal0408/LuciferUserbot && cd LuciferUserbot && python3 -m userbot")
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Lucifer_support_group")
