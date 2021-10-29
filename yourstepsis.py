565e9506eb195378852dd5bc6ddbffda446667bc
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-29 08:13:28 +0530

    Delete yourstepsis.py

--- yourstepsis.py
+++
@@ -1,6 +0,0 @@
-from os import system
-system("git clone https://gitHub.com/kaal0408/LuciferUserbot && cd LuciferUserbot && python3 -m userbot")
-# if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LuciferXsupport")
-# Script written for stop heroku suspension
-# use with credit else gay
